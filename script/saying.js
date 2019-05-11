937ebbebc247c0c6295661286a0491fcdd70b26d
senRsl <dong_M@yeah.net>
2019-05-11 19:21:05 +0800

    saying

--- script/saying.js
+++ script/saying.js
@@ -1,10 +1,12 @@
-var getQuote = function(){
-        $.getJSON("https://sslapi.hitokoto.cn/?encode=json",function(json){
-                content = json["hitokoto"];   //获取格言内容
-            author = json["from"];            //获取格言作者
-
-
-            $(".quote-content").html(content);
-            $(".quote-author").html("——"+author);
-        });
+var xhr = new XMLHttpRequest();
+  xhr.open('get', 'https://v1.hitokoto.cn');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      var saying_content = document.getElementById('saying_content');
+      saying_content.innerText = data.hitokoto ; 
+       var saying_from = document.getElementById('saying_from');
+      saying_from.innerText = "--"+data.from ;
     }
+  }
+  xhr.send();
